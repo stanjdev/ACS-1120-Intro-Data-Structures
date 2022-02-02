73c69c0f1a1befdcf04d89701ff6d6709b7cab44
Stanley Jeong <stanley.jeong@shopify.com>
2022-02-01 20:43:14 -0800

    displays sampled word

--- Code/app.py
+++ Code/app.py
@@ -1,25 +1,34 @@
 """Main script, uses other modules to generate sentences."""
-from flask import Flask
-# from histogram import histdict my thing!
-
+from flask import Flask, render_template, url_for
+from histogram import histogram_dict, read_file
+from sample import sampler, test_sampler
+file = './data/socrates-apology.txt'
 
 app = Flask(__name__)
-
 
 @app.before_first_request
 def before_first_request():
     """Runs only once at Flask startup"""
     # TODO: Initialize your histogram, hash table, or markov chain here.
+    corpus_list = read_file(file)
+    histogram = histogram_dict(corpus_list)
+    return histogram
 
 
 @app.route("/")
 def home():
+    corpus_list = read_file(file).replace(',', '').replace('.', '').replace('?', '').replace('"', '').replace('”', '').replace('’', '').replace('`', '').replace('!', '').replace('/', '').replace(';', '').replace(':', '').lower().split()
+    histogram = histogram_dict(corpus_list)
+    chosen_word = sampler(corpus_list)
     """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here! bla blah, utilizing my sample function!</p>"
+    # return histogram
+    # return "<p>{histogram}</p>"
+    return render_template('index.html', chosen_word=chosen_word, histogram=histogram)
 
 
 if __name__ == "__main__":
-    """To run the Flask server, execute `python app.py` in your terminal.
+    """To run the Flask server, execute `python3 app.py` in your terminal.
        To learn more about Flask's DEBUG mode, visit
        https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
     app.run(debug=True)
+
---
+++ Code/histogram.py
@@ -0,0 +1,102 @@
+# https://makeschool.org/mediabook/oa/tutorials/tweet-generator--data-structures---probability-with-python/analyze-word-frequency-in-text/
+# import sys
+
+# file = str(sys.argv[1])
+
+def read_file(filename):
+  f = open(filename, 'r', encoding='utf-8-sig')
+  words = f.read()
+  f.close()
+  return words
+
+def histogram_dict(source_text):
+  """return a histogram in dictionary/hash table form
+  source_text: string
+  """
+  dict = {}
+  for word in source_text:
+    if word in dict.keys():
+      dict[word] += 1
+    else:
+      dict[word] = 1
+  return dict
+
+
+
+def histogram_list(source_text):
+  lst = []
+  
+  # # THIS WORKS, but I can't explain why...
+  for word in source_text:
+    for pair in lst:
+      if word == pair[0]:
+        pair[1] = pair[1] + 1
+        break
+    else: 
+      lst.append([word, 1])
+
+  # # python implementation: https://stackoverflow.com/questions/48774616/return-the-value-of-a-matching-item-in-a-python-list
+  # for word in source_text:
+  #   found_pair = next((pair for pair in lst if word in pair), None)
+  #   if found_pair is not None:
+  #     found_pair[1] += 1
+  #   else: lst.append([word, 1])
+  return lst
+
+
+
+# NEEDS IMPROVEMENT - immutable data structure.
+def histogram_tuple(source_text):
+  tuple_list = []
+  for word in source_text:
+    found_pair = next((pair for pair in tuple_list if word in pair), None)
+    if found_pair is not None:
+      tuple_list.remove(found_pair)
+      amount = found_pair[1]
+      temp_list = list(found_pair)
+      temp_list[1] = amount + 1
+      found_pair = tuple(temp_list)
+      tuple_list.append(found_pair)
+    else: tuple_list.append((word, 1))
+  return tuple_list
+
+
+def unique_words(dict):
+  return f'{len(dict)} unique words'
+  # returns the total count of unique words in the histogram. For example, when given the histogram for The Adventures of Sherlock Holmes, it returns the integer 8475.
+
+""" 
+word:
+histogram: 
+ """
+def frequency(word, histogram):
+  # for lists
+  for pair in histogram:
+    if pair[0] == word:
+      return pair[1]
+
+  # for dictionaries
+  if word in histogram.keys():
+    return histogram[word]
+
+  # returns the number of times that word appears in a text. For example, when given the word "mystery" and the Holmes histogram, it will return the integer 20.
+
+
+
+# if __name__ == '__main__':
+#   corpus_list = read_file(file).replace(',', '').replace('.', '').replace('?', '').replace('"', '').replace('”', '').replace('’', '').replace('!', '').replace('/', '').replace(';', '').lower().split()
+
+  # histogram_dictionary = histogram_dict(corpus_list)
+  # print(histogram_dictionary)
+#   print(unique_words(histogram_dictionary))
+#   print(frequency('sherlock', histogram_dictionary))
+
+  # hist_list = histogram_list(corpus_list)
+  # print(hist_list)
+  # print(unique_words(hist_list))
+  # print(frequency('sherlock', hist_list))
+
+  # hist_tuple = histogram_tuple(corpus_list)
+  # print(hist_tuple)
+  # print(unique_words(hist_tuple))
+
