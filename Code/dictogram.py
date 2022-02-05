8a66f8baa280d6ee80ecc63208ab8d0bbbd59375
Stanley Jeong <stanley.jeong@shopify.com>
2022-02-04 17:15:26 -0800

    ready to submit to gradescope dictogram.py

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -22,36 +22,34 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
-        if word in self.keys():
+        if word in self:
             self[word] += count
-            self.tokens += count
         else: 
             self[word] = count
             self.types += 1
-            self.tokens += count
+        self.tokens += count # everytime we run add_count() will always increment token regardless of if/else
 
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
-        if word in self.keys():
-            return self[word]
-        else:
-            return 0
+        return self.get(word, 0)
+        # SAME AS
+        # if word in self.keys():
+        #     return self[word]
+        # else:
+        #     return 0
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
-        histogram = self
-        length = len(histogram)
-        dart = random.randint(1, length)
-
         target = 0
-        for key in histogram:
-            target += histogram[key]
+        dart = random.uniform(0, self.tokens)
+        for word, count in self.items():
+            target += count
             if target >= dart:
-                return key
+                return word
 
 
 def print_histogram(word_list):
