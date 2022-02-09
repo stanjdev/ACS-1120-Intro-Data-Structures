ab96b6cb85593ccfc35aca63ea8e664f00b6f69c
Stanley Jeong <stanley.jeong@shopify.com>
2022-02-09 12:18:21 -0800

    markov attempt 1

--- Code/practice.py
+++ Code/practice.py
@@ -1,19 +1,15 @@
+# class MyClass(dict):
+#     def __init__(self, word_list = None):
+#         super(MyClass, self).__init__()
 
-from unicodedata import name
-
-
-class MyClass(dict):
-    def __init__(self, word_list = None):
-        super(MyClass, self).__init__()
-
-        if word_list is not None:
-            for word in word_list:
-                print(self)
+#         if word_list is not None:
+#             for word in word_list:
+#                 print(self)
 
 
 
-test = MyClass(['var', 'rsa', 'esf', 'fgsdfg', 'agaga', ])
+# test = MyClass(['var', 'rsa', 'esf', 'fgsdfg', 'agaga', ])
 
-if __name__ == '__main__':
-    print(test)
+# if __name__ == '__main__':
+#     print(test)
 
